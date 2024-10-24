eadea17a785b83872a5b35b59e9f1bdb254c2f19
Shreyansh Gupta <hardikshrey123@gmail.com>
2024-10-25 00:01:57 +0530

    push

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',  // Adjust based on your backend setup
+    baseURL: process.env.API_URL,  // Adjust based on your backend setup
     headers: {
         'Content-Type': 'application/json'
     }
